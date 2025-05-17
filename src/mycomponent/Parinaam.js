4d0fa0c78acca8dee6a89dcc256310ca52adf285
Jay Jadhao <jadhaojay190@gmail.com>
2025-05-17 18:36:57 +0530

    Create Parinaam.js

---
+++ src/mycomponent/Parinaam.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Parinaam = () => {
+  return (
+    <footer className='bg-dark text-light my-5 py-3'>
+      <h4 className='text-center'>Ch*d gaye guru</h4>
+  
+    </footer>
+  );
+};
+
+export default Parinaam;
